368fe332b0cbc44f0cfed45664dabbcf7da87e96
Kristianb23 <76174180+Kristianb23@users.noreply.github.com>
2021-05-28 14:45:04 +0200

    spørgsmål og css

---
+++ quizquestions.js
@@ -0,0 +1,58 @@
+let questions = [
+    {
+    numb: 1,
+    question: "In which view will you be able to see your data-sets?",
+    answer: "In the plot view",
+    options: [
+      "In the plot view",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 2,
+    question: "If you want to measure from 0° to 360° to find out what the antennas horizontal range is, what scan type would you use?",
+    answer: "A principal scan, with plane set on azimuth",
+    options: [
+      "tomorrow",
+      "A principal scan, with plane set on elevation",
+      "A principal scan, with plane set on azimuth",
+      "today"
+    ]
+  },
+    {
+    numb: 3,
+    question: "if you want to measure from 0° to 60° to find out what the antennas vertical range is, what scan type would you use?",
+    answer: "A principal scan, with plane set on elevation",
+    options: [
+      "A principal scan, with plane set on azimuth",
+      "today",
+      "today",
+      "A principal scan, with plane set on elevation"
+    ]
+  },
+    {
+    numb: 4,
+    question: "How do you get the cross polar data?",
+    answer: "By matching it with the co polar data.",
+    options: [
+      "tomorrow",
+      "By matching it with the co polar data.",
+      "today",
+      "today"
+    ]
+  },
+    {
+    numb: 5,
+    question: "Where is the speed between points decided?",
+    answer: "When exporting the flight",
+    options: [
+      "When exporting the flight",
+      "today",
+      "today",
+      "today"
+    ]
+  },
+
+];
